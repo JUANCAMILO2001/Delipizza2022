5c4a18509e8c6696b068844964e6f9b8b172e762
Camilo_Dev.JR <juan.camilorodrigez@gmail.com>
2022-11-05 15:36:44 -0500

    modificacion iframe a oscuro

--- resources/views/layouts/components/contact/content.blade.php
+++ resources/views/layouts/components/contact/content.blade.php
@@ -6,7 +6,7 @@
             <section class=" no-padding dark-bg hidden-section">
                 <div class="map-container">
                     <div id="singleMap">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15908.337307348946!2d-74.1592163!3d4.5788768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1667437279792!5m2!1ses-419!2sco" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"  ></iframe>
+                        <iframe style="filter: invert(90%);" width="100%" height="100%"  src="https://maps.google.com/maps?width=100%&amp;height=600&amp;q=4.5900197,-74.1542900&amp;iwloc=B&amp;output=embed"></iframe>
                     </div>
 
                 </div>
